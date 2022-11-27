e64b2e9a4cb2d88787c706e48d0049ad40315e6e
BOYDREAMIN <101074004+BOYDREAMIN@users.noreply.github.com>
2022-11-27 23:18:44 +0900

    Add files via upload

---
+++ Scientific_Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Scientific_Calculator
+{
+    class Scientific_Calculator
+    {
+        public static void Main()
+        {
+            MathFunc m = new MathFunc();
+
+            Console.Write(m.PI());
+        }
+    }
+}
