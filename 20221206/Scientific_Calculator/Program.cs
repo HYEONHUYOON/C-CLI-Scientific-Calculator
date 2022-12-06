bdf1659b740413e9bc0cc2312ac6ec68d5c3ecbc
BOYDREAMIN <101074004+BOYDREAMIN@users.noreply.github.com>
2022-12-07 01:23:35 +0900

    Add files via upload

---
+++ 20221206/Scientific_Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Scientific_Calculator
+{
+    class Scientific_Calculator
+    {
+        public static void Main()
+        {
+            MathFunc mathF = new MathFunc();
+
+            Console.Write(mathF.exp(1, 1));
+
+            Calculator cal = new Calculator();
+            while (true)
+            {
+                cal.Print();
+
+                if (!cal.isSecondMode)
+                {
+                    cal.BasicUI();
+                }
+                else
+                {
+                    cal.SecondModeUI();
+                }
+
+                cal.InputNum();
+            }
+        }
+    }
+}
